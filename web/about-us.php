37aac1417f6fed8905749f0af65de8d3b665f7a4
icohen1013 <shender1314@gmail.com>
2016-09-13 16:15:33 +0800

    0.0.0
    複製cake-house-master的文件

---
+++ web/about-us.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Cake House</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<div id="Header">
+  <div class="Container">
+    <div id="MenuIcon"><a class="DB" href="#"><span class="SrOnly">menu icon</span></a></div>
+    <h1 id="LOGO"><a class="DB" href="index.php"><span class="SrOnly">Cake House</span></a></h1>
+
+    <!-- 會員相關區塊 start-->
+    <div id="MemberContent">
+      <div id="MemberIcon"><a href="#" class="DB"><span class="SrOnly">會員登入</span></a></div>
+      <div id="CartIcon"><a href="#" class="DB"><span class="SrOnly">購物車</span></a></div>
+    </div>
+    <!-- 會員相關區塊 end-->
+
+    <nav id="PrimaryNavigation">
+      <ul>
+        <li><a href="#" class="DB"><span>關於我們</span></a></li>
+        <li><a href="#" class="DB"><span>最新消息</span></a></li>
+        <li><a href="#" class="DB"><span>產品介紹</span></a></li>
+        <li><a href="#" class="DB"><span>聯絡我們</span></a></li>
+      </ul>
+    </nav>
+  </div>
+</div>
+<div id="Banner">banner</div>
+<div id="News">
+  <div class="Container">news</div>
+</div>
+<div class="Container">container</div>
+<div id="Taste">
+  <div class="Container">taste</div>
+</div>
+<!-- 頁尾 -->
+<div id="Footer">
+  <div class="Container">footer</div>
+</div>
+</body>
+</html>
